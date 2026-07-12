b413aab83848e0cadde660304bcfd36a7bb80f80
Genta1215 <popopoz0410@gmial.com>
2026-07-12 17:07:26 +0900

    fix: oil.nvimをインストールし、ファイルの詳細を表示するキーマップを追加

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -133,5 +133,41 @@ return {
       require('config.peek')
     end,
   },
+
+  -- -------------------------------------------------------
+  -- ファイルエクスプローラー
+  -- -------------------------------------------------------
+
+  -- https://github.com/stevearc/oil.nvim
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+    config = function()
+      local detail = false,
+      require("oil").setup({
+        -- ファイルの詳細情報を表示するキーマップを追加 (https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#toggle-file-detail-view)
+        keymaps = {
+          ["gd"] = {
+            desc = "Toggle file detail view",
+            callback = function()
+              detail = not detail
+              if detail then
+                require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+              else
+                require("oil").set_columns({ "icon" })
+              end
+            end,
+          },
+        },
+      })
+    end
+  }
 }
 
