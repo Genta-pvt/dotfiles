ae1d776e55d3747d92b1067f955c345e14ba3f45
Genta1215 <popopoz0410@gmial.com>
2026-07-15 07:17:00 +0900

    fix: oli.nvimの設定変更(trash,winbar)

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -153,8 +153,27 @@ return {
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     config = function()
-      local detail = false,
+      -- Declare a global function to retrieve the current directory
+      -- winbar に CWD を表示するための関数
+      function _G.get_oil_winbar()
+        local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+        local dir = require("oil").get_current_dir(bufnr)
+        if dir then
+          return vim.fn.fnamemodify(dir, ":~")
+        else
+          -- If there is no current directory (e.g. over ssh), just show the buffer name
+          return vim.api.nvim_buf_get_name(0)
+        end
+      end
+
+      local detail = false, -- ファイルの詳細情報表示で使う
       require("oil").setup({
+        -- ゴミ箱の使用
+        delete_to_trash = true,
+        -- winbarにCWDを表示
+        win_options = {
+          winbar = "%!v:lua.get_oil_winbar()",
+        },
         -- ファイルの詳細情報を表示するキーマップを追加 (https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#toggle-file-detail-view)
         keymaps = {
           ["gd"] = {
